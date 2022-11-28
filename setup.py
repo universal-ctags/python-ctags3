1e4318697b296de9cdeb1f3a9c89a122658e55b6
Jonas Haag <jonas@lophus.org>
2022-11-28 10:33:57 +0100

    CI: Add more Python versions

--- setup.py
+++ setup.py
@@ -41,7 +41,6 @@ setup(
         'Operating System :: POSIX',
         'Topic :: Software Development :: Libraries :: Python Modules',
         "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
